e2cf6b71c166dc559aeacf968d263fff1a561e71
Christian Mayer <christian@fox21.at>
2025-04-23 08:53:59 +0200

    Build

--- bin/build.sh
+++ bin/build.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-zig build --verbose --release=small -Dtarget=aarch64-macos
-zig build --verbose --release=small -Dtarget=x86_64-linux
+zig build --verbose --summary all --release=small -Dtarget=aarch64-macos
+zig build --verbose --summary all --release=small -Dtarget=x86_64-linux
 #aarch64-linux x86_64-windows
