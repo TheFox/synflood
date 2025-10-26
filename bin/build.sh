dc31f4280e2f7d05be67d3e1403be97be9394643
Christian Mayer <christian@fox21.at>
2025-10-26 16:14:12 +0100

    Build

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-for target in aarch64-macos x86_64-linux ; do
-    echo "-> target: ${target}"
-    zig build --verbose --summary all --release=small -Dtarget=${target}
-done
+zig build --verbose --summary all --release
